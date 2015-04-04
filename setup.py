4477a90940cf1af15b871b2d8cf03b662932732e
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2015-04-04 10:56:07 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='specipy',
+    version='1.0',
+    packages=['specipy'],
+    url='https://github.com/oarrabi/specipy',
+    license='MIT',
+    author='omarsubhiabdelhafith',
+    author_email='o.arrabi@me.com',
+    description=''
+)
---
+++ specipy/__init__.py
@@ -0,0 +1,2 @@
+def a_func():
+    print "the value is bla"
